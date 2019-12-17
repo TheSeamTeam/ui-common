c7bbfb88abc14ed760b81037e472db58ff4c6c67
Mark Berry <markberry867@gmail.com>
2019-12-17 15:03:33 -0600

    Add hasHeader input to widget

--- projects/ui-common/src/lib/widget/widget/widget.stories.ts
+++ projects/ui-common/src/lib/widget/widget/widget.stories.ts
@@ -1,134 +1,157 @@
 import { action } from '@storybook/addon-actions'
 import { boolean, select, text, withKnobs } from '@storybook/addon-knobs'
 import { linkTo } from '@storybook/addon-links'
-import { storiesOf } from '@storybook/angular'
+import { moduleMetadata, storiesOf } from '@storybook/angular'
 
+import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { faWrench } from '@fortawesome/free-solid-svg-icons'
 
 import { TheSeamWidgetModule } from '../widget.module'
+import { WidgetComponent } from './widget.component'
 
-storiesOf('Components/Widget', module)
-  .addDecorator(withKnobs)
-
-  .add('Simple', () => ({
-    moduleMetadata: {
+export default {
+  title: 'Components/Widget',
+  component: WidgetComponent,
+  decorators: [
+    moduleMetadata({
       imports: [
-        TheSeamWidgetModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        BrowserModule,
+        TheSeamWidgetModule
       ]
-    },
-    props: {
-      icon: faWrench,
-      title: text('Header Title', 'Example Widget'),
-      loading: boolean('Loading', false)
-    },
-    template: `
-      <div class="p-4" style="height: 400px; width: 500px;">
-        <seam-widget [icon]="icon" [titleText]="title" [loading]="loading">
-          Widget Body
-        </seam-widget>
-      </div>`
-  }))
+    }),
+    withKnobs
+  ],
+  parameters: {
+    docs: {
+      iframeHeight: '300px',
+    }
+  }
+}
 
-  .add('FontAwesome Icon', () => ({
-    moduleMetadata: {
-      imports: [
-        TheSeamWidgetModule,
-        BrowserAnimationsModule
-      ]
-    },
-    props: {
-      icon: faWrench,
-    },
-    template: `
-      <div class="p-4" style="height: 400px; width: 500px;">
-        <seam-widget [icon]="icon" titleText="Example Widget">
-          Widget Body
-        </seam-widget>
-      </div>`
-  }))
+export const Simple = () => ({
+  props: {
+    icon: faWrench,
+    title: text('Header Title', 'Example Widget'),
+    loading: boolean('Loading', false)
+  },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget [icon]="icon" [titleText]="title" [loading]="loading">
+        Widget Body
+      </seam-widget>
+    </div>`
+})
 
-  .add('Image Icon', () => ({
-    moduleMetadata: {
-      imports: [
-        TheSeamWidgetModule,
-        BrowserAnimationsModule
-      ]
-    },
-    props: {
-      icon: 'assets/images/icons8-pass-fail-32.png'
-    },
-    template: `
-      <div class="p-4" style="height: 400px; width: 500px;">
-        <seam-widget [icon]="icon" titleText="Example Widget">
-          Widget Body
-        </seam-widget>
-      </div>`
-  }))
+Simple.story = {
+  name: 'Simple'
+}
 
-  .add('Title Template', () => ({
-    moduleMetadata: {
-      imports: [
-        TheSeamWidgetModule,
-        BrowserAnimationsModule
-      ]
-    },
-    props: {
-      icon: faWrench,
-    },
-    template: `
-      <div class="p-4" style="height: 400px; width: 500px;">
-        <seam-widget [icon]="icon">
-          <ng-template seamWidgetTitleTpl>
-            Example Widget
-            <span class="badge float-right text-light badge-success">
-              complete
-            </span>
-          </ng-template>
-          Widget Body
-        </seam-widget>
-      </div>`
-  }))
+export const FAIcon = () => ({
+  props: {
+    icon: faWrench,
+  },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget [icon]="icon" titleText="Example Widget">
+        Widget Body
+      </seam-widget>
+    </div>`
+})
 
-  .add('Icon Template', () => ({
-    moduleMetadata: {
-      imports: [
-        TheSeamWidgetModule,
-        BrowserAnimationsModule
-      ]
-    },
-    props: { },
-    template: `
-      <div class="p-4" style="height: 400px; width: 500px;">
-        <seam-widget titleText="Example Widget">
-          <ng-template seamWidgetIconTpl>
-            <span class="border border-danger">
-              <img src="assets/images/icons8-pass-fail-32.png">
-            </span>
-          </ng-template>
-          Widget Body
-        </seam-widget>
-      </div>`
-  }))
+FAIcon.story = {
+  name: 'FontAwesome Icon'
+}
 
-  .add('Loading', () => ({
-    moduleMetadata: {
-      imports: [
-        TheSeamWidgetModule,
-        BrowserAnimationsModule
-      ]
-    },
-    props: {
-      icon: faWrench,
-      title: text('Header Title', 'Example Widget'),
-      loading: boolean('Loading', true)
-    },
-    template: `
-      <div class="p-4" style="height: 400px; width: 500px;">
-        <seam-widget [icon]="icon" [titleText]="title" [loading]="loading">
-          Widget Body
-        </seam-widget>
-      </div>`
-  }))
+export const ImageIcon = () => ({
+  props: {
+    icon: 'assets/images/icons8-pass-fail-32.png'
+  },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget [icon]="icon" titleText="Example Widget">
+        Widget Body
+      </seam-widget>
+    </div>`
+})
+
+ImageIcon.story = {
+  name: 'Image Icon'
+}
+
+export const TitleTemplate = () => ({
+  props: {
+    icon: faWrench,
+  },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget [icon]="icon">
+        <ng-template seamWidgetTitleTpl>
+          Example Widget
+          <span class="badge float-right text-light badge-success">
+            complete
+          </span>
+        </ng-template>
+        Widget Body
+      </seam-widget>
+    </div>`
+})
+
+TitleTemplate.story = {
+  name: 'Title Template'
+}
+
+export const IconTemplate = () => ({
+  props: { },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget titleText="Example Widget">
+        <ng-template seamWidgetIconTpl>
+          <span class="border border-danger">
+            <img src="assets/images/icons8-pass-fail-32.png">
+          </span>
+        </ng-template>
+        Widget Body
+      </seam-widget>
+    </div>`
+})
+
+IconTemplate.story = {
+  name: 'Icon Template'
+}
+
+export const Loading = () => ({
+  props: {
+    icon: faWrench,
+    title: text('Header Title', 'Example Widget'),
+    loading: boolean('Loading', true)
+  },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget [icon]="icon" [titleText]="title" [loading]="loading">
+        Widget Body
+      </seam-widget>
+    </div>`
+})
+
+Loading.story = {
+  name: 'Loading'
+}
+
+export const NoHeader = () => ({
+  props: {
+    loading: boolean('Loading', false)
+  },
+  template: `
+    <div class="p-4" style="height: 270px; width: 500px;">
+      <seam-widget [hasHeader]="false" [loading]="loading">
+        Widget Body
+      </seam-widget>
+    </div>`
+})
+
+NoHeader.story = {
+  name: 'No Header'
+}
