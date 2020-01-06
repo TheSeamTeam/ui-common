b762ff121dffc088a1b9ae0a832d88e30f0de75a
Mark Berry <markberry867@gmail.com>
2020-01-06 15:51:09 -0600

    Fix sid-nav anchor items content not center vertically

--- projects/ui-common/src/lib/datatable/index.ts
+++ projects/ui-common/src/lib/datatable/index.ts
@@ -3,6 +3,7 @@ export * from './datatable/datatable.component'
 
 export * from './models/table-column'
 export * from './models/preferences'
+export * from './models/preferences-accessor'
 
 export * from './tokens/datatable-preferences-accessor'
 
