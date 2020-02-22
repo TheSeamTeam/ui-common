7194791bdf1c25b8e6bb7bcceb169ca30ed0cf43
Mark Berry <markberry867@gmail.com>
2020-02-22 05:17:21 -0600

    Add more options to table-cell-type schematic schema
    These options don't work yet, but the schematic doesn't yet either.

--- projects/ui-common/schematics/table-cell-type/schema.ts
+++ projects/ui-common/schematics/table-cell-type/schema.ts
@@ -7,4 +7,38 @@ export interface Schema {
 
   // The name of the project.
   project?: string
+
+  displayBlock?: boolean
+
+  inlineStyle?: boolean
+
+  inlineTemplate?: boolean
+
+  viewEncapsulation?: string
+
+  changeDetection?: string
+
+  prefix?: string
+
+  style?: string
+
+  type?: string
+
+  skipTests?: boolean
+
+  flat?: boolean
+
+  skipImport?: boolean
+
+  selector?: string
+
+  skipSelector?: boolean
+
+  module?: string
+
+  export?: boolean
+
+  entryComponent?: boolean
+
+  lintFix?: boolean
 }
