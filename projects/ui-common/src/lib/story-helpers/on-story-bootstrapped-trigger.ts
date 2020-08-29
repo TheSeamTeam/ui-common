f271bfa1d4059f060f9a984e0803232f81113eb4
Mark Berry <markberry867@gmail.com>
2020-08-28 22:26:32 -0500

    Add onStoryBootstrapped functions

---
+++ projects/ui-common/src/lib/story-helpers/on-story-bootstrapped-trigger.ts
@@ -0,0 +1,41 @@
+import { APP_BOOTSTRAP_LISTENER, ComponentRef, ElementRef } from '@angular/core'
+
+// TODO: Consider supporting a target selection function, so that complex
+// selections that can't be expressed by a simple selector can be used.
+
+/**
+ * Can be used to trigger an event on a target element when the story component has been bootstrapped.
+ *
+ * Example:
+ * ```
+ * export const TouchExample1 = () => ({
+ *   moduleMetadata: {
+ *     providers: [ onStoryBootstrappedTrigger('input', 'blur') ]
+ *   },
+ *   props: { control: new FormControl('') },
+ *   template: `
+ *     <input type="text" [formControl]="control">
+ *     Touched: {{ control.touched }}
+ *   `
+ * })
+ * ```
+ */
+export function onStoryBootstrappedTrigger(targetSelector: string, eventName: string) {
+  function bootstrapped(componentRef: ComponentRef<any>) {
+    const elementRef = componentRef.injector.get(ElementRef, null)
+    const target = elementRef?.nativeElement.querySelector(targetSelector)
+    if (!target) {
+      console.warn(`Unable to trigger event '${eventName}'. Target '${targetSelector}' not found.`)
+    }
+
+    const e = document.createEvent('HTMLEvents')
+    e.initEvent('blur', false, true)
+    target.dispatchEvent(e)
+  }
+
+  return {
+    provide: APP_BOOTSTRAP_LISTENER,
+    useValue: bootstrapped,
+    multi: true
+  }
+}
