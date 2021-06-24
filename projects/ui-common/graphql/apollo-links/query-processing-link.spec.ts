388bf000c4be55b57abfe893d5e5a7977df8fc03
Mark Berry <markberry867@gmail.com>
2021-06-24 02:45:19 -0500

    Update query-processing-link tests

--- projects/ui-common/graphql/apollo-links/query-processing-link.spec.ts
+++ projects/ui-common/graphql/apollo-links/query-processing-link.spec.ts
@@ -41,34 +41,12 @@ export const testResultLink = new ApolloLink(operation => {
 })
 
 describe('GraphQL apollo-links queryProcessingLink', () => {
-  it('should execute link', () => {
-    const query = gql`
-      # @gql-rule: remove-not-defined
-      query TestQuery ($search: String) {
-        example {
-          totalCount
-          items {
-            thing1
-            thing2
-          }
-        }
-      }
-    `
 
-    const link =
-      // printOperationLink({ tag: 'Before', styles: 'color:cyan' })
-      // .concat(queryProcessingLink)
-      queryProcessingLink
-      // .concat(printOperationLink({ tag: 'After', styles: 'color:limegreen' }))
-      .concat(testResultLink)
-
-    ApolloLink.execute(link, {
-      query,
-      // variables: { search: 'test' },
-      variables: { search: undefined }
-    }).subscribe(v => {
-      const expectedQuery = gql`
-        query TestQuery {
+  describe('Rule "remove-not-defined"', () => {
+    it('should work on OperationDefinition node', () => {
+      const query = gql`
+        # @gql-rule: remove-not-defined
+        query TestQuery ($search: String) {
           example {
             totalCount
             items {
@@ -79,7 +57,81 @@ describe('GraphQL apollo-links queryProcessingLink', () => {
         }
       `
 
-      expect(print(v.data?.operation.query)).toBe(print(expectedQuery))
+      const link =
+        // printOperationLink({ tag: 'Before', styles: 'color:cyan' })
+        // .concat(queryProcessingLink)
+        queryProcessingLink
+        // .concat(printOperationLink({ tag: 'After', styles: 'color:limegreen' }))
+        .concat(testResultLink)
+
+      ApolloLink.execute(link, {
+        query,
+        // variables: { search: 'test' },
+        variables: { search: undefined }
+      }).subscribe(v => {
+        const expectedQuery = gql`
+          query TestQuery {
+            example {
+              totalCount
+              items {
+                thing1
+                thing2
+              }
+            }
+          }
+        `
+
+        expect(print(v.data?.operation.query)).toBe(print(expectedQuery))
+      })
     })
   })
+
+  describe('Rule "inline-variable"', () => {
+    it('should work on VariableDefinition node', () => {
+      const query = gql`
+        query TestQuery (
+          # @gql-rule: inline-variable
+          $search: String
+        ) {
+          example(where: $where) {
+            totalCount
+            items {
+              thing1
+              thing2
+            }
+          }
+        }
+      `
+
+      const link =
+        // printOperationLink({ tag: 'Before', styles: 'color:cyan' })
+        // .concat(queryProcessingLink)
+        queryProcessingLink
+        // .concat(printOperationLink({ tag: 'After', styles: 'color:limegreen' }))
+        .concat(testResultLink)
+
+      ApolloLink.execute(link, {
+        query,
+        variables: { where: { and: [ { thing1: { eq: 'test' } } ] } },
+        // variables: { where: undefined }
+      }).subscribe(v => {
+        const expectedQuery = gql`
+          query TestQuery {
+            example {
+              totalCount
+              items {
+                thing1
+                thing2
+              }
+            }
+          }
+        `
+
+        expect(print(v.data?.operation.query)).toBe(print(expectedQuery))
+      })
+  })
+
+  // describe('Rule "remove-not-used"', () => {
+
+  // })
 })
