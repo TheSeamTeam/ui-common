66d546e69078129a7266c72e1b72f0a4bad52640
Mark Berry <markberry867@gmail.com>
2021-07-29 20:44:47 -0500

    Start adding new nav-item parsing

--- projects/ui-common/framework/side-nav/side-nav.service.spec.ts
+++ projects/ui-common/framework/side-nav/side-nav.service.spec.ts
@@ -11,49 +11,74 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing'
 import { Router } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
+import { createRoutingFactory, SpectatorRouting } from '@ngneat/spectator/jest'
 
 import { TheSeamSideNavService } from './side-nav.service'
 
-@Component({ template: `Url: {{ router.url }}` })
+@Component({ template: `<router-outlet></router-outlet>` })
 class TestPlacholderComponent {
   constructor(public router: Router) { }
 }
 
 describe('TheSeamSideNavService', () => {
-  let service: TheSeamSideNavService
+  // let service: TheSeamSideNavService
 
-  let location: Location
-  let router: Router
+  // let location: Location
+  // let router: Router
 
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule.withRoutes([
-          { path: 'foo', component: TestPlacholderComponent },
-        ])
-      ],
-      providers: [ TheSeamSideNavService ]
-    })
-    service = TestBed.inject(TheSeamSideNavService)
+  // beforeEach(async () => {
+  //   TestBed.configureTestingModule({
+  //     imports: [
+  //       RouterTestingModule.withRoutes([
+  //         { path: 'foo', component: TestPlacholderComponent },
+  //       ])
+  //     ],
+  //     providers: [ TheSeamSideNavService ]
+  //   })
+  //   service = TestBed.inject(TheSeamSideNavService)
 
-    router = TestBed.inject(Router)
-    location = TestBed.inject(Location)
+  //   router = TestBed.inject(Router)
+  //   location = TestBed.inject(Location)
+  // })
+
+  // afterEach(() => {
+  //   jest.clearAllMocks()
+
+  //   // tslint:disable: max-line-length
+  //   // Necessary to avoid this error "Provided value for `preserveWhitespaces` can not be changed once it has been set." :
+  //   // Source: https://github.com/angular/angular/commit/e342ffd855ffeb8af7067b42307ffa320d82177e#diff-92b125e532cc22977b46a91f068d6d7ea81fd61b772842a4a0212f1cfd875be6R28
+  //   // tslint:enable: max-line-length
+  //   ɵresetJitOptions()
+  // })
+
+
+  let spectator: SpectatorRouting<TestPlacholderComponent>
+  const createComponent = createRoutingFactory({
+    component: TestPlacholderComponent,
+    providers: [ TheSeamSideNavService ],
+    routes: [
+      { path: '', component: TestPlacholderComponent },
+      { path: 'foo', component: TestPlacholderComponent },
+    ]
   })
 
-  afterEach(() => {
-    jest.clearAllMocks()
-
-    // Necessary to avoid this error "Provided value for `preserveWhitespaces` can not be changed once it has been set." :
-    // Source: https://github.com/angular/angular/commit/e342ffd855ffeb8af7067b42307ffa320d82177e#diff-92b125e532cc22977b46a91f068d6d7ea81fd61b772842a4a0212f1cfd875be6R28
-    ɵresetJitOptions()
-  })
+  beforeEach(() => spectator = createComponent())
 
   it('should be created', () => {
+    const service = spectator.inject(TheSeamSideNavService)
     expect(service).toBeTruthy()
   })
 
   it('should be active', () => {
-    const state = service.getItemsState([])
-    expect(state).toBeTruthy()
+    const service = spectator.inject(TheSeamSideNavService)
+    const items = service.getItemsState([
+      {
+        itemType: 'link',
+        link: '/foo'
+      }
+    ])
+    const item = items[0]
+
+    expect(item.__state).toBe(true)
   })
 })
--- projects/ui-common/framework/side-nav/side-nav.service.ts
+++ projects/ui-common/framework/side-nav/side-nav.service.ts
@@ -3,9 +3,9 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router'
 import { combineLatest, Observable, of } from 'rxjs'
 import { filter, map, startWith, switchMap } from 'rxjs/operators'
 
-import { activatedRoutesWithDataProperty, IActivatedRouteWithData, routeSnapshotPathFull } from '@theseam/ui-common/utils'
+import { activatedRoutesWithDataProperty, IActivatedRouteWithData, notNullOrUndefined, routeSnapshotPathFull } from '@theseam/ui-common/utils'
 
-import { ISideNavItem, SideNavItemWithState } from './side-nav.models'
+import { ISideNavBasic, ISideNavItem, ISideNavLink, SideNavItemState, SideNavItemWithState } from './side-nav.models'
 
 @Injectable({ providedIn: 'root' })
 export class TheSeamSideNavService {
@@ -18,7 +18,38 @@ export class TheSeamSideNavService {
   }
 
   public getItemsState(items: ISideNavItem[]): SideNavItemWithState[] {
-    return []
+    return items.map(itm => this._getItemWithState(itm))
+  }
+
+  private _getItemWithState(item: ISideNavItem): SideNavItemWithState {
+    const state: SideNavItemState = {
+      active: false
+    }
+
+    const children = this._getItemChildren(item)
+
+    const withState: SideNavItemWithState = {
+      ...item,
+      __state: state
+    }
+
+    if (this._canHaveChildren(withState) && children !== null) {
+      withState.children = children.map(c => this._getItemWithState(c))
+    }
+
+    return withState
+  }
+
+  private _getItemChildren(item: ISideNavItem): ISideNavItem[] | null {
+    if (this._canHaveChildren(item) && notNullOrUndefined(item.children)) {
+      return item.children
+    }
+
+    return null
+  }
+
+  private _canHaveChildren(item: ISideNavItem): item is (ISideNavBasic | ISideNavLink) {
+    return item.itemType === 'basic' || item.itemType === 'link'
   }
 
 }
