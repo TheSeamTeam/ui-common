33db72ec24d934a64cf9e0ae516d1443a0eb89a2
Mark Berry <markberry867@gmail.com>
2021-09-29 18:09:15 -0500

    Minor refactor and documentation update to subscriberCount()

--- projects/ui-common/utils/subscriber-count.ts
+++ projects/ui-common/utils/subscriber-count.ts
@@ -1,27 +1,44 @@
 import { Observable, Subscriber } from 'rxjs'
 
-declare let window: any
-
 /**
  * This is just for helping debug observables that aren't being unsubscribed
  * from correctly.
+ *
+ * If description is 'foo' then you can check the how many observers are
+ * subscribed with the expression `__subscriberCounts['foo']`. In a debugger you
+ * can add the expression to the "Watch" expressions or in Chrome Devtools
+ * Console you can add it to the live expressions(Add live expressions by
+ * clicking the eye the the left of the Console filter input).
  */
-export function subscriberCount<T>(sourceObservable: Observable<T>, description: string) {
+export function subscriberCount<T>(sourceObservable: Observable<T>, description: string, logSubscriptions: boolean = true) {
   let counter = 0
   return new Observable((subscriber: Subscriber<T>) => {
     const subscription = sourceObservable.subscribe(subscriber)
     counter++
-    console.log(`${description} subscriptions: ${counter}`)
-    if (window.__subscriberCounts === undefined) {
-      window.__subscriberCounts = {}
+    if (logSubscriptions) {
+      console.log(`${description} subscriptions: ${counter}`)
     }
-    window.__subscriberCounts[description] = counter
+    setGlobalSubscriberCount(description, counter)
 
     return () => {
       subscription.unsubscribe()
       counter--
-      console.log(`${description} subscriptions: ${counter}`)
-      window.__subscriberCounts[description] = counter
+      if (logSubscriptions) {
+        console.log(`${description} subscriptions: ${counter}`)
+      }
+      setGlobalSubscriberCount(description, counter)
     }
   })
+}
+
+function getGlobalSubscriberCounts(): { [description: string]: number } {
+  const w = window as any
+  if (w.__subscriberCounts === undefined || w.__subscriberCounts === null) {
+    w.__subscriberCounts = {}
+  }
+  return w.__subscriberCounts
+}
+
+function setGlobalSubscriberCount(description: string, count: number): void {
+  getGlobalSubscriberCounts()[description] = count
 }
