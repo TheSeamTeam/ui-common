ddaff698429243127b89ccfdf19dabaa33e838da
Mark Berry <mark.berry@theseam.com>
2022-06-20 17:31:36 -0500

    Improve map editing

--- projects/ui-common/google-maps/google-maps.service.ts
+++ projects/ui-common/google-maps/google-maps.service.ts
@@ -2,6 +2,11 @@ import { Injectable, NgZone, ViewContainerRef } from '@angular/core'
 
 import { MapManagerService } from '@theseam/ui-common/map'
 import { isNullOrUndefined, notNullOrUndefined } from '@theseam/ui-common/utils'
+import booleanContains from '@turf/boolean-contains'
+import {
+  multiPolygon as turfjsMultiPolygon,
+  polygon as turfjsPolygon,
+} from '@turf/helpers'
 
 declare const ngDevMode: boolean | undefined
 
@@ -23,6 +28,58 @@ const DEFAULT_DRAWING_MANAGER_OPTIONS: (() => google.maps.drawing.DrawingManager
   polygonOptions: DEFAULT_POLYGON_OPTIONS,
   drawingMode: null
 })
+
+const FEATURE_STYLE_OPTIONS_DEFAULT: google.maps.Data.StyleOptions = {
+  clickable: true,
+  visible: true,
+  // zIndex?: number;
+
+  // cursor?: string;
+  draggable: false,
+  editable: false,
+  fillColor: 'gray',
+  fillOpacity: 0.3,
+  strokeColor: 'gray',
+  strokeOpacity: 1,
+  strokeWeight: 2,
+}
+
+const FEATURE_STYLE_OPTIONS_SELECTED: google.maps.Data.StyleOptions = {
+  ...FEATURE_STYLE_OPTIONS_DEFAULT,
+  draggable: true,
+  editable: true,
+  fillColor: 'green',
+  fillOpacity: 0.7,
+  strokeColor: 'limegreen',
+  strokeOpacity: 1,
+  strokeWeight: 2,
+}
+
+const APP_FEATURE_PROPERTY_PREFIX = '__app__'
+
+function getSeamFeaturePropertyName(name: string): string {
+  return `${APP_FEATURE_PROPERTY_PREFIX}${name}`
+}
+
+function getSeamFeatureProperty(feature: google.maps.Data.Feature, name: string): any {
+  return feature.getProperty(getSeamFeaturePropertyName(name))
+}
+
+function setSeamFeatureProperty(feature: google.maps.Data.Feature, name: string, value: any): void {
+  feature.setProperty(getSeamFeaturePropertyName(name), value)
+}
+
+function isFeatureSelected(feature: google.maps.Data.Feature): boolean {
+  const isSelected = getSeamFeatureProperty(feature, 'isSelected')
+  if (typeof isSelected === 'boolean') {
+    return isSelected
+  }
+  return false
+}
+
+function setFeatureSelected(feature: google.maps.Data.Feature, isSelected: boolean): void {
+  setSeamFeatureProperty(feature, 'isSelected', isSelected)
+}
 
 type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] }
 
@@ -46,6 +103,27 @@ export class GoogleMapsService {
     this._initDrawingManager()
     this._initFeatureStyling()
     this._initFeatureChangeListeners()
+
+    window.addEventListener('keypress', (event: KeyboardEvent) => {
+      console.log('code', event.code)
+    })
+
+    window.addEventListener('keydown', (event: KeyboardEvent) => {
+      console.log('code', event.code)
+
+      if (event.code === 'Delete') {
+        this._assertInitialized()
+        this.googleMap.data.forEach(f => {
+          if (isFeatureSelected(f)) {
+            this.googleMap?.data.remove(f)
+          }
+        })
+      } else if (event.code === 'Escape') {
+        if (this._drawingManager?.getDrawingMode() !== null) {
+          this._drawingManager?.setDrawingMode(null)
+        }
+      }
+    })
   }
 
   private _initDrawingManager() {
@@ -61,6 +139,18 @@ export class GoogleMapsService {
     drawingManager.setMap(this.googleMap)
 
     this._drawingManager = drawingManager
+
+    this._drawingManager.addListener('drawingmode_changed', event => {
+      // console.log('drawingmode change', event, this._drawingManager?.getDrawingMode())
+      if (this._drawingManager?.getDrawingMode() !== null) {
+        this._assertInitialized()
+        this.googleMap.data.forEach(f => {
+          if (isFeatureSelected(f)) {
+            setFeatureSelected(f, false)
+          }
+        })
+      }
+    })
   }
 
   public addPolygonEditorControls(element: HTMLElement): void {
@@ -94,40 +184,63 @@ export class GoogleMapsService {
   private _initFeatureStyling(): void {
     this._assertInitialized()
 
+    this.googleMap.addListener('click', (even: google.maps.MapMouseEvent | google.maps.IconMouseEvent) => {
+      this._assertInitialized()
+      console.log('%cMap click', 'color:cyan')
+      this.googleMap.data.forEach(f => setFeatureSelected(f, false))
+    })
+
     // Color each letter gray. Change the color when the isColorful property
     // is set to true.
     this.googleMap.data.setStyle((feature) => {
-      console.log('setStyle', feature)
-      let color = 'gray'
-      if (feature.getProperty('isColorful')) {
-        color = feature.getProperty('color')
+      // console.log('setStyle', feature)
+      // let color = 'gray'
+      // if (feature.getProperty('isColorful')) {
+      //   color = feature.getProperty('color')
+      // }
+
+      if (isFeatureSelected(feature)) {
+        return FEATURE_STYLE_OPTIONS_SELECTED
       }
-      return /** @type {!google.maps.Data.StyleOptions} */({
-        fillColor: color,
-        strokeColor: color,
-        strokeWeight: 2
-      })
+
+      // return /** @type {!google.maps.Data.StyleOptions} */({
+      //   fillColor: color,
+      //   strokeColor: color,
+      //   strokeWeight: 2
+      // })
+      return FEATURE_STYLE_OPTIONS_DEFAULT
     })
 
     // When the user clicks, set 'isColorful', changing the color of the letters.
     this.googleMap.data.addListener('click', (event: google.maps.Data.MouseEvent) => {
       console.log('click', event)
-      event.feature.setProperty('isColorful', true)
+      // event.feature.setProperty('isColorful', true)
       console.log('type', event.feature.getGeometry().getType())
+
+      this._assertInitialized()
+
+      setFeatureSelected(event.feature, true)
+      this.googleMap.data.forEach(f => {
+        if (f !== event.feature && isFeatureSelected(f)) {
+          setFeatureSelected(f, false)
+        }
+      })
     })
 
     // When the user hovers, tempt them to click by outlining the letters.
     // Call revertStyle() to remove all overrides. This will use the style rules
     // defined in the function passed to setStyle()
     this.googleMap.data.addListener('mouseover', (event: google.maps.Data.MouseEvent) => {
-      console.log('mouseover', event)
+      // console.log('mouseover', event)
       this._assertInitialized()
       this.googleMap.data.revertStyle()
-      this.googleMap.data.overrideStyle(event.feature, {strokeWeight: 8})
+      if (!this.isDrawing() && !isFeatureSelected(event.feature)) {
+        this.googleMap.data.overrideStyle(event.feature, { strokeWeight: 4 })
+      }
     })
 
     this.googleMap.data.addListener('mouseout', (event: google.maps.Data.MouseEvent) => {
-      console.log('mouseout', event)
+      // console.log('mouseout', event)
       this._assertInitialized()
       this.googleMap.data.revertStyle()
     })
@@ -137,35 +250,115 @@ export class GoogleMapsService {
     this._assertInitialized()
 
     this.googleMap.data.addListener('addfeature', (event: google.maps.Data.AddFeatureEvent) => {
-      console.log('added feature', event.feature)
+      console.log('%cadded feature', 'color:limegreen', event.feature)
     })
 
     this.googleMap.data.addListener('removefeature', (event: google.maps.Data.RemoveFeatureEvent) => {
-      console.log('removed feature', event.feature)
+      console.log('%cremoved feature', 'color:limegreen', event.feature)
     })
 
     if (notNullOrUndefined(this._drawingManager)) {
-      google.maps.event.addListener(this._drawingManager, 'overlaycomplete', (polygon: google.maps.drawing.OverlayCompleteEvent) => {
-      // google.maps.event.addListener(this._drawingManager, 'polygoncomplete', (polygon: google.maps.drawing.OverlayCompletePolygonEvent) => {
-        console.log('polygon complete', polygon)
+      // google.maps.event.addListener(this._drawingManager, 'overlaycomplete', (event: google.maps.drawing.OverlayCompleteEvent) => {
+      google.maps.event.addListener(this._drawingManager, 'polygoncomplete', (polygon: google.maps.Polygon) => {
+        console.log('%cpolygon complete', 'color:limegreen', polygon)
 
         this._assertInitialized()
-        // polygon.overlay.
-        // console.log(this.googleMap.data.toGeoJson())
-        const arr = (polygon.overlay as google.maps.Polygon).getPath().getArray()
-        this.googleMap.data.add(new google.maps.Data.Feature({
+        const arr = polygon.getPath().getArray()
+        const feature = new google.maps.Data.Feature({
           geometry: new google.maps.Data.Polygon([ arr ])
-          // geometry: polygon.overlay
-        }))
-
-        this.googleMap.data.forEach(f => {
-          // if (f === polygon.overlay) {
-          //   console.log('found')
-          // }
-          console.log('f', f)
         })
+        this.googleMap.data.add(feature)
+        setFeatureSelected(feature, true)
+
+        polygon.setMap(null)
+
+        this._drawingManager?.setDrawingMode(null)
+
+        // this.googleMap.data.forEach(f => {
+        //   if () {
+
+        //   }
+        // })
+
+        let exteriorPolygonFeature: google.maps.Data.Feature | undefined
+        this.googleMap.data.forEach(f => {
+          if (f !== feature && (f.getGeometry().getType() === 'Polygon' && this.featureContains(f, feature))) {
+            exteriorPolygonFeature = f
+          }
+        })
+        console.log('found', exteriorPolygonFeature)
+        if (exteriorPolygonFeature) {
+          const featurePolygon = feature.getGeometry() as google.maps.Data.Polygon
+          const exteriorPolygon = exteriorPolygonFeature.getGeometry() as google.maps.Data.Polygon
+          exteriorPolygonFeature.setGeometry(new google.maps.Data.Polygon([
+            ...exteriorPolygon.getArray(),
+            featurePolygon.getAt(0).getArray().reverse()
+          ]))
+
+          this.googleMap.data.remove(feature)
+          setFeatureSelected(exteriorPolygonFeature, true)
+        }
+
+        this.googleMap.data.toGeoJson(f => console.log('geoJson', f))
       })
     }
+  }
+
+  public isDrawing(): boolean {
+    if (isNullOrUndefined(this._drawingManager)) {
+      return true
+    }
+
+    return this._drawingManager.getDrawingMode() !== null
+  }
+
+  public featureContains(featureA: google.maps.Data.Feature, featureB: google.maps.Data.Feature): boolean {
+    // let aGeoJsonFeature: any
+    // featureA.toGeoJson(f => aGeoJsonFeature = f)
+
+    // let bGeoJsonFeature: any
+    // featureB.toGeoJson(f => bGeoJsonFeature = f)
+
+    const polygonCoordinates = (googlePolygon: google.maps.Data.Polygon): number[][][] => {
+      return googlePolygon.getArray().map(linRing => {
+        const coords = linRing.getArray().map(x => [ x.lng(), x.lat() ])
+        // Google maps paths don't always start and stop at the exact same
+        // position, so this will fix that for turfjs.
+        if (coords.length > 1) {
+          coords.push(coords[0])
+        }
+        return coords
+      })
+    }
+
+    const multiPolygonCoordinates = (googleMultiPolygon: google.maps.Data.MultiPolygon): number[][][][] => {
+      return googleMultiPolygon.getArray().map(x => polygonCoordinates(x))
+    }
+
+    const toTurfJsPolygon = (googlePolygon: google.maps.Data.Polygon) => {
+      return turfjsPolygon(polygonCoordinates(googlePolygon))
+    }
+
+    const toTurfJsMultiPolygon = (googleMultiPolygon: google.maps.Data.MultiPolygon) => {
+      return turfjsMultiPolygon(multiPolygonCoordinates(googleMultiPolygon))
+    }
+
+    const toTurfJsFeature = (googleFeature: google.maps.Data.Feature) => {
+      if (googleFeature.getGeometry().getType() === 'Polygon') {
+        return toTurfJsPolygon(googleFeature.getGeometry() as google.maps.Data.Polygon)
+      } else if (googleFeature.getGeometry().getType() === 'MultiPolygon') {
+        return toTurfJsMultiPolygon(googleFeature.getGeometry() as google.maps.Data.MultiPolygon)
+      }
+
+      throw Error(`Unexpected geometry.`)
+    }
+
+    const polygonA = toTurfJsFeature(featureA)
+    const polygonB = toTurfJsFeature(featureB)
+
+    console.log('polygonA', polygonA)
+    console.log('polygonB', polygonB)
+    return booleanContains(polygonA, polygonB)
   }
 
   /** Asserts that the map has been initialized. */
