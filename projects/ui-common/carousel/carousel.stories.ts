6e35321523643aebb8c4962b65b1733893ab639c
Mark Berry <mark.berry@theseam.com>
2023-06-01 18:26:47 -0500

    Add carousel harness and story play functions

--- projects/ui-common/carousel/carousel.stories.ts
+++ projects/ui-common/carousel/carousel.stories.ts
@@ -1,106 +1,154 @@
-import { moduleMetadata } from '@storybook/angular'
+import { applicationConfig, Meta, moduleMetadata, StoryObj } from '@storybook/angular'
 
-import { BrowserModule } from '@angular/platform-browser'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { provideAnimations } from '@angular/platform-browser/animations'
+
+import { expectFn, getHarness } from '@theseam/ui-common/testing'
 
 import { TheSeamCarouselComponent } from './carousel.component'
 import { TheSeamCarouselModule } from './carousel.module'
+import { TheSeamCarouselHarness } from './testing'
 
-export default {
+const meta: Meta = {
   title: 'Carousel/Components',
   component: TheSeamCarouselComponent,
   decorators: [
+    applicationConfig({
+      providers: [
+        provideAnimations(),
+      ],
+    }),
     moduleMetadata({
       imports: [
-        BrowserAnimationsModule,
-        BrowserModule,
         TheSeamCarouselModule
       ]
     })
   ],
 }
 
-export const Basic = ({ ...args }) => ({
-  template: `
-    <seam-carousel
-      style="width: 500px; margin: 50px auto;"
-      class="shadow rounded p-2">
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">1</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">2</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">3</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">4</div>
-      </ng-template>
-    </seam-carousel>`
-})
+export default meta
+type Story = StoryObj<TheSeamCarouselComponent>
 
-export const Fast = ({ ...args }) => ({
-  template: `
-    <seam-carousel
-      slideInterval="1000"
-      style="width: 500px; margin: 50px auto;"
-      class="shadow rounded p-2">
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">1</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">2</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">3</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">4</div>
-      </ng-template>
-    </seam-carousel>`
-})
+export const Basic: Story = {
+  render: args => ({
+    props: args,
+    template: `
+      <seam-carousel
+        style="width: 500px; margin: 50px auto;"
+        class="shadow rounded p-2">
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">1</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">2</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">3</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">4</div>
+        </ng-template>
+      </seam-carousel>`
+  }),
+  play: async ({ canvasElement, fixture }) => {
+    const carouselHarness = await getHarness(TheSeamCarouselHarness, { canvasElement, fixture })
+    await expectFn(await carouselHarness.hasPreviousSlideButton()).toBe(true)
+    await expectFn(await carouselHarness.hasNextSlideButton()).toBe(true)
+    await expectFn(await carouselHarness.hasSlideButtons()).toBe(true)
+    await expectFn(await carouselHarness.hasAutoPlayToggleButton()).toBe(true)
+  },
+}
 
-export const AutoplayOff = ({ ...args }) => ({
-  template: `
-    <seam-carousel
-      [autoPlay]="false"
-      style="width: 500px; margin: 50px auto;"
-      class="shadow rounded p-2">
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">1</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">2</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">3</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">4</div>
-      </ng-template>
-    </seam-carousel>`
-})
+export const Fast: Story = {
+  render: args => ({
+    props: args,
+    template: `
+      <seam-carousel
+        slideInterval="1000"
+        style="width: 500px; margin: 50px auto;"
+        class="shadow rounded p-2">
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">1</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">2</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">3</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">4</div>
+        </ng-template>
+      </seam-carousel>`
+  })
+}
 
-export const NoButtons = ({ ...args }) => ({
-  template: `
-    <seam-carousel
-      [showPager]="false"
-      [showNavButtons]="false"
-      [showPauseButton]="false"
-      style="width: 500px; margin: 50px auto;"
-      class="shadow rounded p-2">
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">1</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">2</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">3</div>
-      </ng-template>
-      <ng-template seamCarouselSlide>
-        <div class="text-center p-4">4</div>
-      </ng-template>
-    </seam-carousel>`
-})
+export const AutoplayOff: Story = {
+  render: args => ({
+    props: args,
+    template: `
+      <seam-carousel
+        [autoPlay]="false"
+        style="width: 500px; margin: 50px auto;"
+        class="shadow rounded p-2">
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">1</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">2</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">3</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">4</div>
+        </ng-template>
+      </seam-carousel>`
+  }),
+  play: async ({ canvasElement, fixture }) => {
+    const carouselHarness = await getHarness(TheSeamCarouselHarness, { canvasElement, fixture })
+    await expectFn(await carouselHarness.activeTileIndex()).toBe('0')
+    await carouselHarness.goToNextSlide()
+    await expectFn(await carouselHarness.activeTileIndex()).toBe('1')
+    await carouselHarness.goToSlide(3)
+    await expectFn(await carouselHarness.activeTileIndex()).toBe('3')
+    await carouselHarness.goToNextSlide()
+    await expectFn(await carouselHarness.activeTileIndex()).toBe('0')
+    await carouselHarness.goToPreviousSlide()
+    await expectFn(await carouselHarness.activeTileIndex()).toBe('3')
+    await carouselHarness.goToSlide(0)
+    await expectFn(await carouselHarness.activeTileIndex()).toBe('0')
+  },
+}
+
+export const NoButtons: Story = {
+  render: args => ({
+    props: args,
+    template: `
+      <seam-carousel
+        [showPager]="false"
+        [showNavButtons]="false"
+        [showPauseButton]="false"
+        style="width: 500px; margin: 50px auto;"
+        class="shadow rounded p-2">
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">1</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">2</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">3</div>
+        </ng-template>
+        <ng-template seamCarouselSlide>
+          <div class="text-center p-4">4</div>
+        </ng-template>
+      </seam-carousel>`
+  }),
+  play: async ({ canvasElement, fixture }) => {
+    const carouselHarness = await getHarness(TheSeamCarouselHarness, { canvasElement, fixture })
+    await expectFn(await carouselHarness.hasPreviousSlideButton()).toBe(false)
+    await expectFn(await carouselHarness.hasNextSlideButton()).toBe(false)
+    await expectFn(await carouselHarness.hasSlideButtons()).toBe(false)
+    await expectFn(await carouselHarness.hasAutoPlayToggleButton()).toBe(false)
+  },
+}
