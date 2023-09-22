91b697a5f880f9951c4b8eb7e1d30937e3622f8d
Mark Berry <mark.berry@theseam.com>
2023-09-22 12:30:37 -0500

    Remove unused input

--- projects/ui-common/framework/dashboard/dashboard-widgets/dashboard-widget-portal-outlet.directive.ts
+++ projects/ui-common/framework/dashboard/dashboard-widgets/dashboard-widget-portal-outlet.directive.ts
@@ -6,7 +6,6 @@ import {
   ElementRef,
   EmbeddedViewRef,
   EventEmitter,
-  Input,
   OnDestroy,
   OnInit,
   Output,
@@ -37,8 +36,6 @@ export class DashboardWidgetPortalOutletDirective extends BasePortalOutlet imple
 
   /** Reference to the currently-attached component/view ref. */
   private _attachedRef?: CdkPortalOutletAttachedRef
-
-  @Input() widgetId: string | undefined | null
 
   constructor(
     private readonly _componentFactoryResolver: ComponentFactoryResolver,
