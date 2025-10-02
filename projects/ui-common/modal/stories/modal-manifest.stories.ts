80a51cc812bc5241935359e376e74b0fca04c7a2
Mark Berry <mark.berry@theseam.com>
2025-10-02 17:01:20 -0500

    Add stories for manifest-style lazy loading

--- projects/ui-common/modal/stories/modal-manifest.stories.ts
+++ projects/ui-common/modal/stories/modal-manifest.stories.ts
@@ -1,22 +1,126 @@
 import { applicationConfig, componentWrapperDecorator, Meta, moduleMetadata, StoryObj } from '@storybook/angular'
 
-import { Component, importProvidersFrom, inject, NgModule } from '@angular/core'
+import { Component, Directive, importProvidersFrom, inject, input, NgModule } from '@angular/core'
 import { provideAnimations } from '@angular/platform-browser/animations'
+import { switchMap } from 'rxjs'
 
 import { TheSeamOverlayScrollbarDirective } from '@theseam/ui-common/scrollbar'
 import { DynamicComponentManifest, TheSeamDynamicComponentLoaderModule } from '@theseam/ui-common/dynamic-component-loader'
 
 import { TheSeamModalModule } from '../modal.module'
 import { Modal } from '../modal.service'
-import { switchMap } from 'rxjs'
 import { ModalRef } from '../modal-ref'
+
+@Directive({
+  selector: '[storyExampleDirective]',
+  exportAs: 'storyExampleDirective',
+  standalone: true,
+})
+class StoryExampleDirective {
+  storyExampleDirective = input<string>('')
+  constructor() {
+    console.log('StoryExampleDirective')
+  }
+  ngOnInit() {
+    console.log('StoryExampleDirective init', this.storyExampleDirective)
+  }
+}
+
+//
+// Basic
+//
 
 @Component({
   selector: 'story-seam-modal-basic',
   styles: [],
   template: `<span>Example</span>`
 })
-class StorySeamModalBasicComponent { }
+class StorySeamModalBasicComponent {
+  private readonly _modalRef = inject(ModalRef<StorySeamModalBasicComponent>)
+  ngOnInit() {
+    console.log('StorySeamModalBasicComponent modalRef', this._modalRef)
+  }
+}
+
+@Component({
+  selector: 'story-seam-modal-basic-example',
+  styles: [],
+  template: `
+    <div class="p-4">
+      <button type="button" class="btn btn-lightgray" (click)="open()">Open</button>
+    </div>
+  `
+})
+class StorySeamModalBasicExampleComponent {
+  private readonly _modal = inject(Modal)
+
+  open() {
+    this._modal.openFromLazyComponent('basic-modal').pipe(
+      switchMap(mr => mr.afterClosed()),
+    ).subscribe(v => console.log('result', v))
+  }
+}
+
+@NgModule({
+  imports: [
+    TheSeamModalModule,
+    TheSeamOverlayScrollbarDirective,
+    TheSeamDynamicComponentLoaderModule.forChild(StorySeamModalBasicComponent),
+  ],
+})
+class StorySeamModalBasicExampleModule { }
+
+//
+// Basic Non Sa
+//
+
+@Component({
+  selector: 'story-seam-modal-basic-non-sa',
+  styles: [],
+  template: `<span [storyExampleDirective]="'Example'" #tmp="storyExampleDirective">Example</span>[{{ tmp.storyExampleDirective() }}]`,
+  standalone: false,
+})
+class StorySeamModalBasicNonSaComponent {
+  private readonly _modalRef = inject(ModalRef<StorySeamModalBasicNonSaComponent>)
+
+  ngOnInit() {
+    console.log('StorySeamModalBasicNonSaComponent modalRef', this._modalRef)
+  }
+}
+
+@Component({
+  selector: 'story-seam-modal-basic-non-sa-example',
+  styles: [],
+  template: `
+    <div class="p-4">
+      <button type="button" class="btn btn-lightgray" (click)="open()">Open</button>
+    </div>
+  `,
+})
+class StorySeamModalBasicNonSaExampleComponent {
+  private readonly _modal = inject(Modal)
+
+  open() {
+    this._modal.openFromLazyComponent('basic-non-sa-modal').pipe(
+      switchMap(mr => mr.afterClosed()),
+    ).subscribe(v => console.log('result', v))
+  }
+}
+
+@NgModule({
+  imports: [
+    TheSeamModalModule,
+    TheSeamOverlayScrollbarDirective,
+    StoryExampleDirective,
+    TheSeamDynamicComponentLoaderModule.forChild(StorySeamModalBasicNonSaComponent),
+  ],
+  declarations: [StorySeamModalBasicNonSaComponent],
+})
+class StorySeamModalBasicNonSaExampleModule { }
+
+//
+// Simple
+//
 
 @Component({
   selector: 'story-seam-modal-simple',
@@ -43,26 +147,7 @@ class StorySeamModalBasicComponent { }
 class StorySeamModalSimpleComponent {
   private readonly _modalRef = inject(ModalRef<StorySeamModalSimpleComponent>)
   ngOnInit() {
-    console.log('modalRef', this._modalRef)
-  }
-}
-
-@Component({
-  selector: 'story-seam-modal-basic-example',
-  styles: [],
-  template: `
-    <div class="p-4">
-      <button type="button" class="btn btn-lightgray" (click)="open()">Open</button>
-    </div>
-  `
-})
-class StorySeamModalBasicExampleComponent {
-  private readonly _modal = inject(Modal)
-
-  open() {
-    this._modal.openFromLazyComponent('basic-modal').pipe(
-      switchMap(mr => mr.afterClosed()),
-    ).subscribe(v => console.log('result', v))
+    console.log('StorySeamModalSimpleComponent modalRef', this._modalRef)
   }
 }
 
@@ -89,19 +174,14 @@ class StorySeamModalSimpleExampleComponent {
   imports: [
     TheSeamModalModule,
     TheSeamOverlayScrollbarDirective,
-    TheSeamDynamicComponentLoaderModule.forChild(StorySeamModalBasicComponent),
-  ],
-})
-class StorySeamModalBasicExampleModule { }
-
-@NgModule({
-  imports: [
-    TheSeamModalModule,
-    TheSeamOverlayScrollbarDirective,
     TheSeamDynamicComponentLoaderModule.forChild(StorySeamModalSimpleComponent),
   ],
 })
 class StorySeamModalSimpleExampleModule { }
+
+//
+// Manifests
+//
 
 const manifests: DynamicComponentManifest[] = [
   {
@@ -109,6 +189,11 @@ const manifests: DynamicComponentManifest[] = [
     path: 'basic-modal',
     // loadChildren: () => Promise.resolve(StorySeamModalBasicComponent),
     loadChildren: () => Promise.resolve(StorySeamModalBasicExampleModule),
+  },
+  {
+    componentId: 'basic-non-sa-modal',
+    path: 'basic-non-sa-modal',
+    loadChildren: () => Promise.resolve(StorySeamModalBasicNonSaExampleModule),
   },
   {
     componentId: 'simple-modal',
@@ -149,6 +234,19 @@ export const Basic: Story = {
     },
     props: args,
     template: `<story-seam-modal-basic-example></story-seam-modal-basic-example>`,
+  }),
+}
+
+export const BasicNonSa: Story = {
+  render: args => ({
+    moduleMetadata: {
+      imports: [
+        // StorySeamModalBasicNonSaComponent,
+        StorySeamModalBasicNonSaExampleComponent,
+      ],
+    },
+    props: args,
+    template: `<story-seam-modal-basic-non-sa-example></story-seam-modal-basic-non-sa-example>`,
   }),
 }
 
