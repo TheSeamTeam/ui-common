7a283f03c34864f29cba3115eca63e29c95148cd
Mark Berry <markberry867@gmail.com>
2026-03-15 21:59:57 -0500

    feat(route-transitions): export public API from framework entrypoint

---
+++ projects/ui-common/framework/route-transitions/index.ts
@@ -0,0 +1,4 @@
+export { computeDirection, RouteDirection } from './compute-direction'
+export { getUrlSegments } from './get-url-segments'
+export { seamRouteTransition } from './seam-route-transition'
+export { SeamRouteShellComponent } from './seam-route-shell.component'
