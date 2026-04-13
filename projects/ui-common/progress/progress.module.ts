16cec16a2e26cc05c72daa95cbdadb6f968c90f2
Mark Berry <mark.berry@theseam.com>
2026-04-13 12:13:55 -0500

    feat(progress): export segmented-progress-bar from progress entry point

--- projects/ui-common/progress/progress.module.ts
+++ projects/ui-common/progress/progress.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
 import { ProgressCircleComponent } from './progress-circle/progress-circle.component'
+import { TheSeamSegmentedProgressBarComponent } from './segmented-progress-bar/segmented-progress-bar.component'
 
 @NgModule({
   declarations: [ProgressCircleComponent],
-  imports: [CommonModule],
-  exports: [ProgressCircleComponent],
+  imports: [CommonModule, TheSeamSegmentedProgressBarComponent],
+  exports: [ProgressCircleComponent, TheSeamSegmentedProgressBarComponent],
 })
 export class TheSeamProgressModule {}
--- projects/ui-common/progress/public-api.ts
+++ projects/ui-common/progress/public-api.ts
@@ -1,2 +1,4 @@
 export * from './progress-circle/progress-circle.component'
+export * from './segmented-progress-bar/segmented-progress-bar.component'
+export * from './segmented-progress-bar/segmented-progress-bar.models'
 export * from './progress.module'
