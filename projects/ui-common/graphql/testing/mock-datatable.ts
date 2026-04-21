306e5f9d07ca37858d1a68e232386175c559cff8
Mark Berry <mark.berry@theseam.com>
2026-04-21 16:35:56 -0500

    feat(graphql): expose refreshRequested through GqlDatatableAccessor and MockDatatable

--- projects/ui-common/graphql/testing/mock-datatable.ts
+++ projects/ui-common/graphql/testing/mock-datatable.ts
@@ -1,5 +1,5 @@
-import { EventEmitter } from '@angular/core'
-import { BehaviorSubject, Observable } from 'rxjs'
+import { EventEmitter, OutputRef } from '@angular/core'
+import { BehaviorSubject, Observable, Subject } from 'rxjs'
 
 import { DataFilterState } from '@theseam/ui-common/data-filters'
 import {
@@ -21,6 +21,20 @@ export class MockDatatable implements GqlDatatableAccessor {
   private readonly _filterStatesSubject = new BehaviorSubject<
     DataFilterState[]
   >([])
+
+  private readonly _refreshSubject = new Subject<void>()
+
+  public readonly refreshRequested: OutputRef<void> = {
+    subscribe: (cb: (value: void) => void) => {
+      const sub = this._refreshSubject.subscribe(() => cb(undefined as void))
+      return { unsubscribe: () => sub.unsubscribe() }
+    },
+  }
+
+  /** Test helper: simulate the refresh button being clicked. */
+  public triggerRefresh(): void {
+    this._refreshSubject.next()
+  }
 
   private _sorts: SortItem[] = []
   private _rows: any[] = []
