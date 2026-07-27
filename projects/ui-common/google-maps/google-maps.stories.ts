e4fbcab8fe9b16c96886645dac4bb5be4befbd00
Mark Berry <mark.berry@theseam.com>
2026-07-27 13:25:17 -0500

    fix:(google-maps): provide animations for map context menu

--- projects/ui-common/google-maps/google-maps.stories.ts
+++ projects/ui-common/google-maps/google-maps.stories.ts
@@ -1,5 +1,6 @@
-import { moduleMetadata } from '@storybook/angular'
+import { applicationConfig, moduleMetadata } from '@storybook/angular'
 
+import { provideAnimations } from '@angular/platform-browser/animations'
 import { CommonModule } from '@angular/common'
 import { FormControl, ReactiveFormsModule } from '@angular/forms'
 
@@ -15,6 +16,9 @@ export default {
   title: 'GoogleMaps/Components',
   // component: TheSeamGoogleMapsComponent,
   decorators: [
+    applicationConfig({
+      providers: [provideAnimations()],
+    }),
     moduleMetadata({
       imports: [CommonModule, TheSeamGoogleMapsModule],
       providers: [
